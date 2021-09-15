7aecdd81b5c54ee4f19b8769a80a0477b4be7a2c
ChrisTran34 <49351111+DatTran34@users.noreply.github.com>
2021-09-15 08:58:22 -0400

    finish

--- main.go
+++ main.go
@@ -1,49 +1,96 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	//"io/ioutil"
-	//"log"
+	"io/ioutil"
+	"log"
+	"os"
+	"text/tabwriter"
+
 	loggly "loggly"
-	//"net/http"
-	//"os"
+	"net/http"
+	"time"
 )
 
 func main() {
 
-	// url := "http://api.football-data.org/v2/competitions/2003"
+	type Competition struct {
+		ID   int `json:"id"`
+		Area struct {
+			ID   int    `json:"id"`
+			Name string `json:"name"`
+		} `json:"area"`
+		Name        string    `json:"name"`
+		Code        string    `json:"code"`
+		Plan        string    `json:"plan"`
+		LastUpdated time.Time `json:"lastUpdated"`
+	}
 
-	// var api_Token = "0d5fb989868f4421bce51517a5bbb62d"
+	type Season struct {
+		ID              int         `json:"id"`
+		StartDate       string      `json:"startDate"`
+		EndDate         string      `json:"endDate"`
+		CurrentMatchday int         `json:"currentMatchday"`
+		Winner          interface{} `json:"winner"`
+	}
 
-	// //resp, err := http.Get(url)
+	type Standings struct {
+		Stage string      `json:"stage"`
+		Type  string      `json:"type"`
+		Group interface{} `json:"group"`
+		Table []struct {
+			Position int `json:"position"`
+			Team     struct {
+				ID       int    `json:"id"`
+				Name     string `json:"name"`
+				CrestURL string `json:"crestUrl"`
+			} `json:"team"`
+			PlayedGames    int         `json:"playedGames"`
+			Form           interface{} `json:"form"`
+			Won            int         `json:"won"`
+			Draw           int         `json:"draw"`
+			Lost           int         `json:"lost"`
+			Points         int         `json:"points"`
+			GoalsFor       int         `json:"goalsFor"`
+			GoalsAgainst   int         `json:"goalsAgainst"`
+			GoalDifference int         `json:"goalDifference"`
+		} `json:"table"`
+	}
 
-	// // Create a new request using http
-	// req, err := http.NewRequest("GET", url, nil)
+	type Data struct {
+		Competition Competition `json:"competition"`
+		Season      Season      `json:"season"`
+		Standings   []Standings `json:"standings"`
+		Message     string      `json:"message"`
+		ErrorCode   int         `json:"errorCode"`
+		Error       int         `json:"error"`
+	}
 
-	// // add authorization header to the req
-	// req.Header.Add("X-Auth-Token", api_Token)
+	url := "http://api.football-data.org/v2/competitions/2021/standings"
 
-	// // Send req using http Client
-	// client := &http.Client{}
-	// resp, err := client.Do(req)
+	var api_Token = "0d5fb989868f4421bce51517a5bbb62d"
 
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+	// Create a new request using http
+	req, err := http.NewRequest("GET", url, nil)
 
-	// defer resp.Body.Close()
+	// add authorization header to the req
+	req.Header.Add("X-Auth-Token", api_Token)
 
-	// body, err := ioutil.ReadAll(resp.Body)
+	// Send req using http Client
+	http_client := &http.Client{}
+	resp, err := http_client.Do(req)
 
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+	defer resp.Body.Close()
 
-	// _, err = os.Stdout.Write(body)
+	body, err := ioutil.ReadAll(resp.Body)
 
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+	var data Data
+	err_ := json.Unmarshal([]byte(body), &data)
+
+	if err_ != nil {
+		panic(err_)
+	}
 
 	var tag string
 	tag = "My-Go-Demo"
@@ -51,15 +98,56 @@ func main() {
 	// Instantiate the client
 	client := loggly.New(tag)
 
-	// Valid EchoSend (message echoed to console and no error returned)
-	err := client.EchoSend("info", "Good morning!")
-	fmt.Println("err:", err)
+	//_, err = os.Stdout.Write(body)
 
-	// Valid Send (no error returned)
-	err = client.Send("error", "Good morning! No echo.")
-	fmt.Println("err:", err)
+	if data.Message != "" {
+		fmt.Println("Message :", data.Message, "Error Code:", data.ErrorCode)
+		client.EchoSend("error", "The football api returned an error message of "+data.Message)
+	} else if data.Error != 0 {
+		fmt.Println("Error :", data.Error)
+		client.EchoSend("error", "The football api returned an error status : "+fmt.Sprint(data.Error))
+	} else {
+		fmt.Println("======================= " + data.Competition.Name + " =======================")
+		fmt.Println("From " + data.Season.StartDate + " to " + data.Season.EndDate)
+		fmt.Println("=======================TABLE=======================")
+		writer := tabwriter.NewWriter(os.Stdout, 0, 8, 1, '\t', tabwriter.AlignRight)
 
-	// Invalid EchoSend -- message level error
-	err = client.EchoSend("blah", "blah")
-	fmt.Println("err:", err)
+		fmt.Fprintln(writer, "# Team\t\t\tPL\tW\tD\tL\tF\tA\tGD\tPts")
+
+		table := data.Standings[0].Table
+
+		for i := 0; i < len(table); i++ {
+			team := table[i]
+			fmt.Fprintln(writer, fmt.Sprint(team.Position)+" "+
+				team.Team.Name+"\t\t\t"+
+				fmt.Sprint(team.PlayedGames)+"\t"+
+				fmt.Sprint(team.Won)+"\t"+
+				fmt.Sprint(team.Draw)+"\t"+
+				fmt.Sprint(team.Lost)+"\t"+
+				fmt.Sprint(team.GoalsFor)+"\t"+
+				fmt.Sprint(team.GoalsAgainst)+"\t"+
+				fmt.Sprint(team.GoalDifference)+"\t"+
+				fmt.Sprint(team.Points))
+		}
+
+		writer.Flush()
+		client.EchoSend("info", "The api returned Competition "+data.Competition.Name+" is held from "+data.Season.StartDate+" to "+data.Season.EndDate)
+	}
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	////////////////////////////////////////////////////////////////
+
+	// 	// Valid EchoSend (message echoed to console and no error returned)
+	// 	err := client.EchoSend("info", "Good morning!")
+	// 	fmt.Println("err:", err)
+
+	// 	// Valid Send (no error returned)
+	// 	err = client.Send("error", "Good morning! No echo.")
+	// 	fmt.Println("err:", err)
+
+	// 	// Invalid EchoSend -- message level error
+	// 	err = client.EchoSend("blah", "blah")
+	// 	fmt.Println("err:", err)
 }
